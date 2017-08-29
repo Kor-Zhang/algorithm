326417e46ab04e1cc02208cb4b1331bc32bedf4a
Kor Zhang <1138829222@qq.com>
2017-08-29 16:22:17 +0800

    定义了dijkstra(迪杰斯特拉)算法接口和其实现类
    定义了dijkstra(迪杰斯特拉)算法接口和其实现类

---
+++ src/com/zk/algorithm/graph/Dijkstra.java
@@ -0,0 +1,32 @@
+package com.zk.algorithm.graph;
+
+import com.zk.algorithm.graph.Dijkstra.DijkstraNode;
+import com.zk.algorithm.list.List;
+
+/**
+ * dijkstra算法实现单源最短路径;
+ * @author Kor_Zhang
+ * 
+ */
+public class Dijkstra implements ShortestPath<DijkstraNode> {
+	
+	
+
+	@Override
+	public List<DijkstraNode> produce(
+			Integer[][] adjacentMatrix, Integer startV) {
+		
+		
+		return null;
+	}
+	
+	/**
+	 * dijkstra节点
+	 * @author Kor_Zhang
+	 *
+	 */
+	public class DijkstraNode{
+		
+		
+	}
+}
